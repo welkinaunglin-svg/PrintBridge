6f31e0603258d05fc27c3eb7d8b3b7bfe34d81a3
welkinaunglin-svg <welkinaunglin@gmail.com>
2026-06-04 18:27:09 +0630

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>PrintBridge Active</Text>
+      <Text>PrintBridge Testing</Text>
     </View>
   );
 }
